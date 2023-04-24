29be1017b2228a05e8fe98e114d55ccce0aa5a5b
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-04-24 12:57:42 -0500

    Completed functions up to snake_to_camel

--- trials.py
+++ trials.py
@@ -2,30 +2,65 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+    for item in items:
+        print(item)
 
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    even_nums = []
+    
+    for num in nums:
+        if num % 2 == 0:
+            even_nums.append(num)
+    return even_nums
 
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+    odd_indices = []
+    
+    for item in items:
+        if item % 2 != 0:
+            odd_indices.append(items[item])
+    return odd_indices
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    i = 1
+    
+    for item in items:
+        print(f"{i}. {item}")
+        i += 1
 
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    
+    nums = []
+    
+    for i in range(start, stop):
+        nums.append(i)
+    return nums    
 
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    chars = []
+    vowel_list = ['a', 'e', 'i', 'o', 'u']
+    str = ""
+    for letter in word:
+        if letter in vowel_list:
+            chars.append('*')
+        else:
+            chars.append(letter)
+    return str.join(chars)
+            
 
 
 def snake_to_camel(string):
+    camel = []
+    
+    for word in string.split('_'):
+        camel.append(f'{word[0].upper()}{word.split(1)}')
+        
+    return ''.join(camel)
     pass  # TODO: replace this line with your code
 
 
